05d0ab55a550074f988898e503153647a27f6a0a
Rahul Prasad <your-email@example.com>
2026-04-30 17:50:40 +0530

    new changes

--- frontend/Student.MVC/Models/DTOs/ApiResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LearningPlatform.StudentService.WebApp.Models.DTOs
-{
-    public class ApiResponseDto<T>
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
-    }
-}
